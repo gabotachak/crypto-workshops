c2b0a2ff857602e0745d53b7efaa0a2316f2bf8a
Gabriel Andres Anzola Tachak <gabriel.anzola@mercadolibre.com.co>
2024-04-05 17:28:17 -0500

    Grille start

---
+++ Workshops/Hill/README.md
@@ -0,0 +1,14 @@
+Implementar el algoritmo de cifrado de bloques de Hill en Python (o en el lenguaje de su elección) utilizando el módulo 26. Asegúrese de validar que la matriz tiene inversa modular, es decir, que el determinante es diferente a 0 y que el determinante sea coprimo con 26, es decir que $\text{gcd}(\text{detA},26)=1$.
+
+El algoritmo debe recibir como entrada para cifrar:
+
+El mensaje en texto claro
+La clave (matriz de 2x2)
+Para decifrar:
+
+Mensaje cifrado
+La clave (matriz de 2x2)
+Cargar en esta tarea un archivo ZIP con lo siguiente:
+
+Archivo Python
+Pequeño video o screenshot en donde se muestre funcionar el algoritmo
---
+++ Workshops/Turning Grille/Grille.py
@@ -0,0 +1,40 @@
+from typing import List
+
+
+class TurningGrilleEncrypter:
+    matrix: List[List[str]] = []
+
+    def __init__(self, matrix: List[List[str]]) -> None:
+        if not matrix:
+            raise ValueError("Matrix must not be empty")
+
+        for i in range(len(matrix)):
+            if len(matrix[i]) != len(matrix):
+                raise ValueError("Matrix must be square")
+
+        self.matrix = matrix
+
+    def get_rotated_matrix(self, times: int = 1, clockwise: bool = True) -> List[List[str]]:
+        times %= 4
+        rotated_matrix = self.matrix
+        for _ in range(times):
+            if clockwise:
+                rotated_matrix = list(zip(*rotated_matrix[::-1]))
+            else:
+                rotated_matrix = list(zip(*rotated_matrix))[::-1]
+            
+        return rotated_matrix
+
+
+with open("Workshops/Turning Grille/Grille.txt") as f:
+    matrix = [list(line.strip()) for line in f]
+
+print(matrix)
+
+encrypter = TurningGrilleEncrypter(matrix)
+
+for i in range(4):
+    rotated_matrix = encrypter.get_rotated_matrix(i)
+    for row in rotated_matrix:
+        print("".join(row))
+    print()
